e65e0a73257dd352193c7397463dc3cbc648a6c4
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2021-09-19 09:06:02 -0400

    created table

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Robert","Hanna",'rdhanna@gmail.com',134000),
+    new SalesPerson("Jannet","Hanna",'jannet.hanna@gmail.com',145000),
+    new SalesPerson("Gordon","Hanna",'gordon.hanna@gmail.com',156000),
+    new SalesPerson("Karalia","Hanna",'karalia.hanna@gmail.com',167000),
+    new SalesPerson("Lukas","Hanna",'lukas.d.hanna@gmail.com',274000)
+
+  ];
+
+
 
   constructor() { }
 
