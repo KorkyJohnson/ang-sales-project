507d641385912765f774394a0b22abd701c93b02
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2021-09-19 16:25:30 -0400

    added bootstrap templates

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,7 +3,7 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html', // templates used to pretty up table
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
